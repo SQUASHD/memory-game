b59850e553afcaa67adbe091ef4552d648bf50f3
Daniel Hjartland <squashical@gmail.com>
2022-11-11 05:58:56 +0100

    Add images to the app Added images to a data-file and then imported them to the gameboard to be mapped to the image component. Added some styling as well.

--- src/components/image-data.js
+++ src/components/image-data.js
@@ -1,11 +1,117 @@
-const imageList = [{
-  name: 'image1',
-  src: './images/artichoke.png',
-  alt: 'image1',
-  id: 1
-}, {
-  name: 'image2',
-  src: 'https://i.imgur.com/1J1Z1Yx.jpg',
-  }]
+import artichoke from "../images/artichoke.png";
+import aubergine from "../images/aubergine.png";
+import bellpepper from "../images/bellpepper.png";
+import broccoli from "../images/broccoli.png";
+import carrot from "../images/carrot.png";
+import cauliflower from "../images/cauliflower.png";
+import celery from "../images/celery.png";
+import chili from "../images/chili.png";
+import corn from "../images/corn.png";
+import cucumber from "../images/cucumber.png";
+import onion from "../images/onion.png";
+import peas from "../images/peas.png";
+import potato from "../images/potato.png";
+import pumpkin from "../images/pumpkin.png";
+import radish from "../images/radish.png";
+import tomato from "../images/tomato.png";
 
-export default imageList
+const imageList = [
+  {
+    id: 1,
+    name: "artichoke",
+    src: artichoke,
+    alt: "artichoke",
+  },
+  {
+    id: 2,
+    name: "aubergine",
+    src: aubergine,
+    alt: "aubergine",
+  },
+  {
+    id: 3,
+    name: "bellpepper",
+    src: bellpepper,
+    alt: "bellpepper",
+  },
+  {
+    id: 4,
+    name: "broccoli",
+    src: broccoli,
+    alt: "broccoli",
+  },
+  {
+    id: 5,
+    name: "carrot",
+    src: carrot,
+    alt: "carrot",
+  },
+  {
+    id: 6,
+    name: "cauliflower",
+    src: cauliflower,
+    alt: "cauliflower",
+  },
+  {
+    id: 7,
+    name: "celery",
+    src: celery,
+    alt: "celery",
+  },
+  {
+    id: 8,
+    name: "chili",
+    src: chili,
+    alt: "chili",
+  },
+  {
+    id: 9,
+    name: "corn",
+    src: corn,
+    alt: "corn",
+  },
+  {
+    id: 10,
+    name: "cucumber",
+    src: cucumber,
+    alt: "cucumber",
+  },
+  {
+    id: 11,
+    name: "onion",
+    src: onion,
+    alt: "onion",
+  },
+  {
+    id: 12,
+    name: "peas",
+    src: peas,
+    alt: "peas",
+  },
+  {
+    id: 13,
+    name: "potato",
+    src: potato,
+    alt: "potato",
+  },
+  {
+    id: 14,
+    name: "pumpkin",
+    src: pumpkin,
+    alt: "pumpkin",
+  },
+  {
+    id: 15,
+    name: "radish",
+    src: radish,
+    alt: "radish",
+  },
+  {
+    id: 16,
+    name: "tomato",
+    src: tomato,
+    alt: "tomato",
+  },
+];
+
+export default imageList;
